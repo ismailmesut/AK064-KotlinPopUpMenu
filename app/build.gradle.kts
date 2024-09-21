93e5ba926698c40ac081c99e406f49d2b3301176
ismailmesut <ismailmesutmujde@gmail.com>
2024-09-21 09:59:14 +0300

    updated: popup menu

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,6 +7,10 @@ android {
     namespace = "com.ismailmesutmujde.kotlinpopupmenu"
     compileSdk = 34
 
+    buildFeatures {
+        viewBinding = true
+    }
+
     defaultConfig {
         applicationId = "com.ismailmesutmujde.kotlinpopupmenu"
         minSdk = 24
--- app/src/main/java/com/ismailmesutmujde/kotlinpopupmenu/MainActivity.kt
+++ app/src/main/java/com/ismailmesutmujde/kotlinpopupmenu/MainActivity.kt
@@ -1,13 +1,42 @@
 package com.ismailmesutmujde.kotlinpopupmenu
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.PopupMenu
+import com.ismailmesutmujde.kotlinpopupmenu.databinding.ActivityMainBinding
 
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var bindingMain : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        bindingMain = ActivityMainBinding.inflate(layoutInflater)
+        val view = bindingMain.root
+        setContentView(view)
+
+        bindingMain.buttonOpenMenu.setOnClickListener {
+
+            val popup = PopupMenu(this@MainActivity, bindingMain.buttonOpenMenu)
+            popup.menuInflater.inflate(R.menu.popup_menu, popup.menu)
+
+            popup.setOnMenuItemClickListener { item ->
+                when(item.itemId) {
+                    R.id.action_delete -> {
+                        Toast.makeText(applicationContext, "Delete is selected.", Toast.LENGTH_SHORT).show()
+                        true
+                    }
+                    R.id.action_edit -> {
+                        Toast.makeText(applicationContext, "Edit is selected.", Toast.LENGTH_SHORT).show()
+                        true
+                    }
+                    else -> false
+                }
+
+            }
+            popup.show()
+
+        }
 
     }
 }
